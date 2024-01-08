cf2dc9d140b22c4dc88f1c0d1621d5491b627459
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-01-08 07:27:30 +0300

    card created to hold list name and it's to do tasks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import './style.css'
-console.log('test')
+//function to create card to hold list name and add task btn
+function createToDoCard(){
+  
+
+     const listName = document.createElement('h2');
+          listName.textContent = 'Gym';
+
+     const iconHolder = document.createElement('div');
+            iconHolder.classList.add('list-icon')     
+
+          const editIcon = document.createElement('i');
+               editIcon.setAttribute('class','fa-regular fa-pen-to-square');
+
+          const deleteIcon =  document.createElement('i');
+               deleteIcon.setAttribute('class',"fa-solid fa-trash")
+     iconHolder.appendChild(editIcon);
+     iconHolder.appendChild(deleteIcon)       
+    
+          let items = [listName,iconHolder]  
+          const fragment = document.createDocumentFragment();
+
+          items.forEach(item => {
+            console.log(fragment)
+            fragment.appendChild(item);
+         });
+
+         return fragment
+
+}
+const divs = document.querySelector('.card');
+divs.appendChild(createToDoCard())
