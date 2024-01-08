d9bf022b24c27821cf75782c88d5cdbace9c717c
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-01-08 07:28:16 +0300

    font and watch script added

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,16 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode:development,
+  mode:"development",
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
