1afa45a64cb25e6c6991f8499c930555e7d8457a
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-01-28 19:58:27 +0300

    task and list object modified using get method

--- src/index.js
+++ src/index.js
@@ -1,23 +1,66 @@
 ///import { Template } from "webpack";
 
+import { formatDistanceToNowStrict,format } from "date-fns";
 //factory function to create task using user input 
-     export function CreateTask(name,detail,priority){
+     export function CreateTask(name,date,detail,priority,complete){
                   return{
                         name:name,
                         detail:detail,
-                        date:function getDate(a,b){
-                              return a - b
+                        get userDeatail(){
+                              if(this.detail == undefined) return ''
+                              let aboutTask = this.detail
+                                  return aboutTask
                         },
+                        date:date,
+                        get realDate(){
+                                 if(this.date == NaN || this.date == undefined){ return format(Date.now(),"EE io LLL yyyy")}
+                              let result = formatDistanceToNowStrict(this.date,{addSuffix:true})
+                              return result
+                        },
+                             
+                              
+                              
+                              
+                        
+                        
                         priority:priority,
-                        complete:false,
+                        get taskFlag(){
+                              let style;
+                              if(this.priority == 'Priority' || this.priority == undefined) return "white"
+
+                              else if(this.priority == 'high'){
+                                   
+                                     return style = 'red'
+                              }
+                              else if(this.priority == 'medium'){
+                                    return style = 'yellow'
+                              }
+                              else if(this.priority == 'low'){
+                                    return style = 'pink'
+                              }
+                        },
+                        complete:complete,
+                        get status(){
+                            let taskStatus = this.complete == true ? 'checked' : ' ';
+                            return taskStatus
+                              
+                        },
+                       
                         id:Date.now().toString()
                    }
             }
 //object to create list name and has array to store related task inside it
-      export function CreateList(name){
+      export function CreateList(name,tasksFinished){
             return{
                   name:name,
                   id:Date.now().toString(),
+                  tasksFinished:tasksFinished,
+                  get count(){
+                      if(this.tasksFinished == undefined) return `Finished task will be counted here ✅ `
+                      else if(this.tasksFinished == 1) return `${this.tasksFinished} task Finished 🎯`
+                        let tasksCompleted =  this.tasksFinished > 1 ? `${this.tasksFinished} tasks finished  🚀 ` : `No task completed yet 📋.`
+                            return tasksCompleted
+                   },
                   tasks:[]
 
             }
@@ -36,7 +79,7 @@
                   
                         <div class="head">
                               <h3>${list.name}</h3>
-                              <h5>1 task completed</h5>
+                              <h5 data-completed-task='${list.id}'>${list.count}</h5>
                         </div>
                         <div class="form-task">
                               <form data-add-task>
@@ -81,29 +124,29 @@
      
           let taskhtml = `
             <div class='task-holder' data-task-holder='${taskArray.indexOf(task)}'>
-                  
+                
             
             <div>
-                  <input type="checkbox" data-task-checkbox  class="task-checkbox">   
-                  <input type="text" name="${task.name}" data-task-name value='${task.name}' >
-                  <button data-show-detail='${taskArray.indexOf(task)}' class="edit-task"><i class="fa-solid fa-pen-to-square"></i></button>
-                  <button data-task-delete class=delete-task><i class="fa-solid fa-trash"></i></button>
+                  <input type="checkbox" name="${task.name}" ${task.status} data-task-checkbox class="task-checkbok" data-checkbox='${taskArray.indexOf(task)}'>   
+                  <input type="text" name="${task.name}" style='background:${task.taskFlag};' data-user-task-name data-new-name='${taskArray.indexOf(task)}'  value='${task.name}'>  
+                  <button data-show-detail='${taskArray.indexOf(task)}'><i class="fa-solid fa-pen-to-square edit-task"></i></button>
+                  <button data-task-delete data-delete-task='${taskArray.indexOf(task)}' class=delete-task><i class="fa-solid fa-trash" data-delete-icon></i></button>
              </div>
 
             <div class="task-detail-holder" data-about-task='${taskArray.indexOf(task)}'> 
                 
                  <span>
                     <input type="date" name="crossfit"  data-task-date data-date-input='${taskArray.indexOf(task)}'>
-                    <select name="prority" data-priority ='${taskArray.indexOf(task)}' >
-                        <option >Priority</option>
+                    <select  name ='priority' class='task-priority-form'  data-priority ='${taskArray.indexOf(task)}' >
+                        <option> set priority </option>
                         <option value="high">Priority 1</option>
                         <option value="medium">Priority 2</option>
                         <option value="low">Priority 3</option>
                    </select>
-                   <p class="due-date" data-due-date = '${taskArray.indexOf(task)}'>2 weeks ago</p>
+                   <p class="due-date" data-due-date ='${taskArray.indexOf(task)}'>${task.realDate}</p>
                 </span>
                 <div>
-                    <textarea name="crossfit" placeholder="detail" data-detail='${taskArray.indexOf(task)}' ></textarea>
+                    <textarea name="crossfit" placeholder="detail" class='task-description' data-detail='${taskArray.indexOf(task)}' >${task.userDeatail}</textarea>
                 </div>
            </div>
            </div>
@@ -126,16 +169,8 @@
                 return dom
        }
 
-      //  export function displayTask(tasksarr,dom){
-      //        let divs = document.createElement('div') 
-      //     taskTemplate(tasksarr).forEach(ele=>{
-      //       let div = document.createElement('div')
-      //       div.appendChild(ele)
-      //       divs.appendChild(div)
-      //     })
-      //     return divs
-          
-      //     }
+     
+      
 
 
      
