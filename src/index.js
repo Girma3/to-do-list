bf43e21c32ef65ac8f85d5171611c3fec39af4f1
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-01-30 10:30:55 +0300

    function added to find object and project name changed to input tag

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { formatDistanceToNowStrict,format } from "date-fns";
                   return{
                         name:name,
                         detail:detail,
-                        get userDeatail(){
+                        get userDetail(){
                               if(this.detail == undefined) return ''
                               let aboutTask = this.detail
                                   return aboutTask
@@ -74,23 +74,37 @@ import { formatDistanceToNowStrict,format } from "date-fns";
 
  export     function cardTemplate(list){
        let div = document.createElement('div');
-            div.dataset.card = `${list.id}`
+            div.dataset.card = `${list.id}`;
+            div.setAttribute('class','card')
            let card = `
                   
                         <div class="head">
-                              <h3>${list.name}</h3>
+                           <div class="project-holder">
+                           <input type="text" data-project-name='${list.id}' name='${list.name}' class="project-name" value='${list.name}'>
+                             
+                              <div class="project-btn-holder">
+                              <button data-edit-name ='${list.id}' ><i class="fa-solid fa-pen-to-square" data-edit-project></i></button>
+                              <button data-delete-card='${list.id}'><i class="fa-solid fa-trash" data-delete-project></i></button>
+                              
+                              </div>
+                           </div>
+
+                        </div>
+
+                              
                               <h5 data-completed-task='${list.id}'>${list.count}</h5>
                         </div>
+
                         <div class="form-task">
                               <form data-add-task>
-                                    <input type="text" id='${list.id}' data-task-name>
+                                    <input type="text" id='${list.id}' data-task-name placeholder = '+ Add Task'>
                                     <div>
                                           <button class="add-task" data-parent-card='${list.id}'>Add</button>
                                           <button class='cancel-task'>cancel</button>
                                     </div>
                               </form>
                         </div>
-                        <div data-task-store='${list.id}'></div>
+                        <div class="task-store" data-task-store='${list.id}'></div>
                   
                   `
                  div.innerHTML = card
@@ -123,30 +137,34 @@ import { formatDistanceToNowStrict,format } from "date-fns";
                   let div = document.createElement('div');
      
           let taskhtml = `
-            <div class='task-holder' data-task-holder='${taskArray.indexOf(task)}'>
+            <div class='task-holder' id="${task.id}" data-task-holder='${task.id}'>
                 
             
-            <div>
-                  <input type="checkbox" name="${task.name}" ${task.status} data-task-checkbox class="task-checkbok" data-checkbox='${taskArray.indexOf(task)}'>   
-                  <input type="text" name="${task.name}" style='background:${task.taskFlag};' data-user-task-name data-new-name='${taskArray.indexOf(task)}'  value='${task.name}'>  
-                  <button data-show-detail='${taskArray.indexOf(task)}'><i class="fa-solid fa-pen-to-square edit-task"></i></button>
-                  <button data-task-delete data-delete-task='${taskArray.indexOf(task)}' class=delete-task><i class="fa-solid fa-trash" data-delete-icon></i></button>
-             </div>
+            <div class="task-head">
+                  <div class="task-head-input">
+                        <input type="checkbox" name="${task.name}" ${task.status} data-task-checkbox class="task-checkbox" data-checkbox='${task.id}'>   
+                        <input type="text" name="${task.name}" style='background:${task.taskFlag};' data-user-task-name data-new-name='${task.id}'  value='${task.name}'>  
+                  </div>
+                  <div class="task-btn-holder">
+                        <button data-show-detail='${task.id}' class="task-edit-btn"><i class="fa-solid fa-pen-to-square edit-task"></i></button>
+                        <button data-task-delete data-delete-task='${task.id}' class = "delete-task-btn"><i class="fa-solid fa-trash" data-delete-icon></i></button>
+                  </div>
+            </div>      
 
-            <div class="task-detail-holder" data-about-task='${taskArray.indexOf(task)}'> 
+            <div class="task-detail-holder" data-about-task='${task.id}'> 
                 
                  <span>
-                    <input type="date" name="crossfit"  data-task-date data-date-input='${taskArray.indexOf(task)}'>
-                    <select  name ='priority' class='task-priority-form'  data-priority ='${taskArray.indexOf(task)}' >
+                    <input type="date" name="crossfit"  data-task-date data-date-input='${task.id}' placeholder='m'>
+                    <select  name ='priority' class='task-priority-form'  data-priority ='${task.id}' >
                         <option> set priority </option>
                         <option value="high">Priority 1</option>
                         <option value="medium">Priority 2</option>
                         <option value="low">Priority 3</option>
                    </select>
-                   <p class="due-date" data-due-date ='${taskArray.indexOf(task)}'>${task.realDate}</p>
+                   <p class="due-date" data-due-date ='${task.id}'>${task.realDate}</p>
                 </span>
                 <div>
-                    <textarea name="crossfit" placeholder="detail" class='task-description' data-detail='${taskArray.indexOf(task)}' >${task.userDeatail}</textarea>
+                    <textarea name="crossfit" placeholder="detail" class='task-description' data-detail='${task.id}' >${task.userDetail}</textarea>
                 </div>
            </div>
            </div>
@@ -168,6 +186,69 @@ import { formatDistanceToNowStrict,format } from "date-fns";
                 
                 return dom
        }
+      
+       /*
+         *function to find object that hold specific task inside it's array we can use the task id to find 
+        * parent object
+        *parameter 1: array of objects
+        *parameter 2: id number
+        *parameter inside: object property(tasks and id) is used if change made remmember to change it
+        *return object
+        *
+        *  
+        */
+       export   function findObj(arr,id){
+           let targetObject;
+            arr.forEach(obj =>{
+               
+                obj.tasks.forEach(task=>{
+                    if(task.id == id){
+                        targetObject = obj
+                    }
+                });
+            });
+            return targetObject
+        }
+        /*
+        *function to find task using id
+        *parameter 1:obj
+        *parameter 2:id
+        *parameter indide: obj tasks and id property used
+        */
+        export function findTask(obj,id){
+            let targetTask;
+                obj.tasks.forEach(task=>{
+                   if((task.id == id) == true){
+                     targetTask = task
+                   }
+                 });
+         
+             return targetTask
+         }
+         //function that accept nodelist find that element and display it and lose others
+        export function hideOthers(arr,domElement){
+            arr.forEach(element => {
+                if(element == domElement){
+                    element.style.display = 'block';
+                }
+                else{
+                    element.style.display = 'none';
+                }
+            });
+          }
+          //function to find object using id from array
+       export   function findObject(arr,id){
+            let targetObject;
+             arr.forEach(obj =>{
+                
+                if(obj.id == id){
+                    targetObject = obj
+                }
+                   
+                 });
+             
+             return targetObject
+         }
 
      
       
